bfa5ed820b39cee8d5005408957622e3761ed8d3
Nathan Champion - Linux <nchampion301@gmail.com>
2022-05-20 21:52:47 -0500

    Everything from before the library tutorial.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include "Config.hpp"
 using namespace std;
 
 int main(int argc, char** argv)
 {
+    const double d = std::stod(argv[1]);
     cout << "Number of arguments: " << argc << endl;
     
     for (int i = 0; i < argc; i++)
         cout << "argument is: " << argv[i] << endl;
+
+    cout << "Version: " << HelloWorld_VERSION_MAJOR << "."
+        << HelloWorld_VERSION_MINOR << endl;
 
     return 0;
 }
