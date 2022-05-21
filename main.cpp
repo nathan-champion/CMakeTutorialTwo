b329ec29cb22fc6e819f82d61256c2f78f7c8349
Nathan Champion - Linux <nchampion301@gmail.com>
2022-05-20 20:51:36 -0500

    Doing the tutorial now from the CMake website.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
-    cout << "whoopie!" << endl;
+    cout << "Number of arguments: " << argc << endl;
+    
+    for (int i = 0; i < argc; i++)
+        cout << "argument is: " << argv[i] << endl;
+
     return 0;
 }
